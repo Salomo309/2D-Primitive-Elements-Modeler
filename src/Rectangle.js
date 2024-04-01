4fad90ea3b936e38828bc1a285d11b802ebec9c7
Onyxcodeotto <110540920+Onyxcodeotto@users.noreply.github.com>
2024-04-02 03:09:50 +0700

    feat: Rectangle

---
+++ src/Rectangle.js
@@ -0,0 +1,38 @@
+
+class Rectangle extends Shape2D{
+
+    /**
+     * @param {Number} width
+     * @param {Number} height
+     * @param {Number} x
+     * @param {Number} y
+     * @param {String} color
+     */
+    constructor(width, height, x, y, color){
+        let offsetX = width/2
+        let offsetY = height/2
+        super(
+            new Vertices(
+                [
+                    x-offsetX, y+offsetY,
+                    x+offsetX, y+offsetY,
+                    x+offsetX, y-offsetY,
+                    x-offsetX, y-offsetY
+                ],
+                color
+        ), 
+            4,
+            [0,1,2, 2,3,0],
+            new Uniforms(new Point(x,y), color))
+        
+    }
+    /**
+     * @override
+     * @param {WebGLRenderingContext} gl
+     * @param {ProgramInfo} program
+     * @returns {None} 
+     */
+    draw(gl,program){
+        
+    }
+}
