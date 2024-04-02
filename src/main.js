a35f6b92637e8a308f03634e4a16fb50a904caf6
Margaretha Olivia <margarethaolivia41@gmail.com>
2024-04-02 23:28:02 +0700

    chore: render square when click the button

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var renderer = new Renderer("#canvas",document)
+document.addEventListener("DOMContentLoaded", function () {
+  var renderer = new Renderer("#canvas", document);
+  console.log("Succedd");
 
-    console.log("Succedd")
-    renderer.addShape(new Square(   1, 0,0, "14AAF5"))
-    renderer.addShape(new Square(1, 0.5, 0, "EA7D70"))
+  const squareBtn = document.getElementById("square-btn");
+  const lineBtn = document.getElementById("line-btn");
+  const rectangleBtn = document.getElementById("rectangle-btn");
+  const polygonBtn = document.getElementById("polygon-btn");
 
-})
-
+  squareBtn.addEventListener("click", function () {
+    renderer.addShape(new Square(1, 0, 0, "14AAF5"));
+    // renderer.addShape(new Square(1, 0.5, 0, "EA7D70"));
+  });
+});
