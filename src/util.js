f04c52ad6ad8dff60796db7c53a3dd0a6fbe29cd
Margaretha Olivia <margarethaolivia41@gmail.com>
2024-04-02 23:10:46 +0700

    style: import button

---
+++ src/util.js
@@ -0,0 +1,7 @@
+const rotateBtn = document.getElementById("rotate-btn");
+const moveBtn = document.getElementById("move-btn");
+const scaleBtn = document.getElementById("scale-btn");
+const addPointBtn = document.getElementById("addpoint-btn");
+const deletePointBtn = document.getElementById("deletepoint-btn");
+const exportBtn = document.getElementById("export-btn");
+const importBtn = document.getElementById("import-btn");
