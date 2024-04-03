fab32d53fd94dadd905fb7877d4bd10c6038300a
Margaretha Olivia <margarethaolivia41@gmail.com>
2024-04-03 18:50:47 +0700

    feat: add line, polygon object to ui & clear canvas

--- src/main.js
+++ src/main.js
@@ -6,9 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const lineBtn = document.getElementById("line-btn");
   const rectangleBtn = document.getElementById("rectangle-btn");
   const polygonBtn = document.getElementById("polygon-btn");
+  const clearBtn = document.getElementById("clear-btn");
+
+  lineBtn.addEventListener("click", function () {
+    renderer.addShape(new Line(0, 0, 0.8, 0.5, "FFFFFF"));
+  });
 
   squareBtn.addEventListener("click", function () {
-    renderer.addShape(new Square(1, 0, 0, "14AAF5"));
-    // renderer.addShape(new Square(1, 0.5, 0, "EA7D70"));
+    renderer.addShape(new Square(1, 0, 0, "0000FF"));
+  });
+
+  rectangleBtn.addEventListener("click", function () {
+    renderer.addShape(new Rectangle(1.5, 1, 0.1, 0, "00FF00"));
+  });
+
+  polygonBtn.addEventListener("click", function () {
+    renderer.addShape(new Polygon([-0.5, -0.5, 0, 0.5, 0.5, -0.5], "FF0000"));
+  });
+
+  clearBtn.addEventListener("click", function () {
+    renderer.clearCanvas();
   });
 });
