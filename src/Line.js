ed1d27092134c42b6ae0211e4506a72ec1a4e4d6
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2024-04-03 14:50:09 +0700

    feat: line (rotate, trasnlate, scale)

---
+++ src/Line.js
@@ -0,0 +1,95 @@
+class Line extends Shape2D {
+    /**
+     * @param {Number} x1
+     * @param {Number} y1
+     * @param {Number} x2
+     * @param {Number} y2
+     * @param {String} color
+     */
+    constructor(x1, y1, x2, y2, color) {
+        const midPoint = new Point((x1 + x2) / 2, (y1 + y2) / 2);
+
+        super(
+            new Vertices(
+                [
+                    x1, y1,
+                    x2, y2
+                ],
+                color
+            ),
+            2,
+            [0, 1],
+            new Uniforms(midPoint, color)
+        );
+    }
+
+    /**
+     * Rotasi garis berdasarkan titik pivot dan sudut rotasi (dalam derajat).
+     * @param {Number} pivotX - Koordinat x titik pivot.
+     * @param {Number} pivotY - Koordinat y titik pivot.
+     * @param {Number} angle - Sudut rotasi (dalam derajat).
+     */
+    rotate(pivotX, pivotY, angle) {
+        // Pivot Point
+        const pivot = new Point(pivotX, pivotY);
+
+        // Orientation, then Rotate
+        const orientation = new Orientation();
+        orientation.rotate(angle);
+
+        // Matrix Rotation
+        const rotationMatrix = orientation.createMatrix();
+
+        // Rotate every point
+        const vertices = this.vertices.vertices;
+        for (let i = 0; i < vertices.length; i++) {
+            const x = vertices[i].getVertex()[0];
+            const y = vertices[i].getVertex()[1];
+
+
+            // Koordinat Relatif terhadap Pivot
+            const relativeX = x - pivotX;
+            const relativeY = y - pivotY;
+
+            // Matriks rotasi dikalikan ke relatif
+            const rotatedX = rotationMatrix[0] * relativeX + rotationMatrix[1] * relativeY;
+            const rotatedY = rotationMatrix[2] * relativeX + rotationMatrix[3] * relativeY;
+
+            vertices[i].setCoordinates(rotatedX + pivotX, rotatedY + pivotY)
+        }
+
+        this.uniform.rotation = orientation;
+        this.uniform.midPoint = pivot;
+    }
+
+    /**
+     * Penskalaan garis berdasarkan faktor penskalaan untuk sumbu x dan y.
+     * @param {Number} scaleX - Faktor penskalaan untuk sumbu x.
+     * @param {Number} scaleY - Faktor penskalaan untuk sumbu y.
+     */
+    scale(scale) {
+        const scaler = new Scaler();
+        scaler.resize(scale, this);
+    }
+
+    /**
+     * Translasi garis berdasarkan pergeseran pada sumbu x dan y.
+     * @param {Number} deltaX - Pergeseran (translasi) pada sumbu x.
+     * @param {Number} deltaY - Pergeseran (translasi) pada sumbu y.
+     */
+    translate(deltaX, deltaY) {
+        const vertices = this.vertices.vertices;
+
+        vertices.forEach(point => {
+            point.setCoordinates(point.coor[0] + deltaX, point.coor[1] + deltaY);
+        });
+    }
+
+    /**
+     * @override
+     * @param {WebGLRenderingContext} gl
+     */
+    drawElements(gl) {
+        gl.drawArrays(gl.LINES, 0, 2);
+    }
+}
