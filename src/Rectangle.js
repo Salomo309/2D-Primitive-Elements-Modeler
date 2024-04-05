ae83a9290ea919b6c21f05e218738ecb26381d2d
Onyxcodeotto <110540920+Onyxcodeotto@users.noreply.github.com>
2024-04-05 19:59:19 +0700

    chore: Restore extend rectangle

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -62,4 +62,51 @@ class Rectangle extends Shape2D{
         return new Rectangle(data.width, data.height, data.x, data.y, data.color);
     }
 
+    extendRectangleWidth(extensionAmount) {
+        let rotationAngle = this.uniform.rotation.degree;
+        console.log(this.uniform.midPoint)
+        let radians = rotationAngle * Math.PI / 180;
+        let cosTheta = Math.cos(radians);
+        let sinTheta = Math.sin(radians);
+
+        // For each vertex of the rectangle
+        this.vertices.vertices.forEach(vertex=> {
+            let relativeX = vertex.coor[0] - this.uniform.midPoint.coor[0]
+            let relativeY = vertex.coor[1] - this.uniform.midPoint.coor[1]
+            let rotatedX = relativeX * cosTheta + relativeY * sinTheta;
+            let rotatedY = -relativeX * sinTheta + relativeY * cosTheta;
+
+            const offsetX = relativeX > 0 ? extensionAmount / 2 : -extensionAmount / 2;
+            const extendedX = rotatedX + offsetX;
+            const newX = this.uniform.midPoint.coor[0] + extendedX * cosTheta - rotatedY * sinTheta;
+            const newY = this.uniform.midPoint.coor[1] + extendedX * sinTheta + rotatedY * cosTheta;
+
+
+            vertex.coor[0] = newX;
+            vertex.coor[1] = newY;
+        }); 
+    }
+    extendRectangleHeight(extensionAmount) {
+        let rotationAngle = this.uniform.rotation.degree;
+        let radians = rotationAngle * Math.PI / 180;
+        let cosTheta = Math.cos(radians);
+        let sinTheta = Math.sin(radians);
+    
+        // For each vertex of the rectangle
+        this.vertices.vertices.forEach(vertex => {
+            let relativeX = vertex.coor[0] - this.uniform.midPoint.coor[0];
+            let relativeY = vertex.coor[1] - this.uniform.midPoint.coor[1];
+            let rotatedX = relativeX * cosTheta + relativeY * sinTheta;
+            let rotatedY = -relativeX * sinTheta + relativeY * cosTheta;
+    
+            const offsetY = relativeY > 0 ? extensionAmount / 2 : -extensionAmount / 2;
+            const extendedY = rotatedY + offsetY;
+            const newX = this.uniform.midPoint.coor[0] + rotatedX * cosTheta - extendedY * sinTheta;
+            const newY = this.uniform.midPoint.coor[1] + rotatedX * sinTheta + extendedY * cosTheta;
+    
+            vertex.coor[0] = newX;
+            vertex.coor[1] = newY;
+        });
+    }
+
 }
